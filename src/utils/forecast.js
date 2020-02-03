7f4e2f2a447c6068886f93e518748a224714926b
Pranav ANEJA <PAneja@mycwt.com>
2020-02-03 23:39:07 +0530

    Updating the weather summary

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to get the right location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + 'There is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain');
+            callback(undefined, body.daily.data[0].summary + 'There is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain. The visibility will be ' + body.daily.data[0].visibility + '% today');
         }
     }
     )
